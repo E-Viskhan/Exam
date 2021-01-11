6c60485749299383c4d0ebb59a9593da73ff50c6
E-Viskhan <61773723+E-Viskhan@users.noreply.github.com>
2021-01-11 16:13:06 +0300

    Update Task 01.py

--- Tasks/Task 01.py
+++ Tasks/Task 01.py
@@ -5,29 +5,14 @@
 # Задаем список чисел
 numbers = [3, 2, 4, 5, 7, 3, 10, 11, 12]
 
-
 # Объявляем функцию
 def check_uniqueness(numbers):
-    # Способ 1
-    # Проходимся циклом по всем элементам и смотрим количество вхождений в список данного элемента
-
-    """
-    for index in numbers:
-        if(numbers.count(index) > 1):
-            return print(f'True {index}')
-        else:
-            print('False')
-    """
-
-    # Способ 2
-    # Функция set - возвращает уникальные элемента в виде множества
-    # С помощью функции set возвращаем уникальные элементы и сравниваем длину уникального списка с исходным
+    
     set_numbers = set(numbers)
     if len(set_numbers) == len(numbers):
-        return print(f'True')
+        return print('True')
     else:
-        return print(f'False')
-
+        return print('False')
 
 # Вызываем функцию
 check_uniqueness(numbers)
